a1598ec4b88e5b05f6ba8e84fb6392d252ec54fb
TFWidgets <widgetstf@gmail.com>
2025-08-17 14:35:21 +0200

    Update config.js

---
+++ src/config.js
@@ -0,0 +1,12 @@
+export const defaultConfig = {
+  locale: 'en-GB',
+  target: null,
+  labels: {
+    days: 'Days',
+    hours: 'Hours', 
+    minutes: 'Minutes',
+    seconds: 'Seconds'
+  },
+  doneText: 'Event started!',
+  autoStart: true
+};
